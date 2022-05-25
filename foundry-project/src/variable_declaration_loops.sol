62d943d58d40001764210e26093da264faa270e3
Sergiu Iacob <sergiu.iacob1@gmail.com>
2022-05-25 16:16:35 +0300

    add files

---
+++ foundry-project/src/variable_declaration_loops.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract VariableLoop {
+    function declareVariableInsideLoop() public pure {
+        for (uint i = 1; i <= 1000; i++) {
+            uint x = i * i;
+        }
+    }
+
+    function declareVariableOutsideLoop() public pure {
+        uint x;
+        for (uint i = 1; i <= 1000; i++) {
+            x = i * i;
+        }
+    }
+}
